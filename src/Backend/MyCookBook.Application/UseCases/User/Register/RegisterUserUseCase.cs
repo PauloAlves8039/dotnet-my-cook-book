aa275d353cf27cad0e878d80cd0fb9328f3bf038
PauloAlves8039 <pj38alves@gmail.com>
2023-05-10 14:25:27 -0300

    Registrando o usuário com validação

---
+++ src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,24 @@
+﻿using MyCookBook.Communication.Requests;
+
+namespace MyCookBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public Task Execute(RequestRegisterUserJson request) 
+        {
+            
+        }
+
+        private void Validate(RequestRegisterUserJson request) 
+        {
+            var validator = new RegisterUserValidator();
+            var result = validator.Validate(request);
+
+            if (result.IsValid) 
+            {
+                var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ src/Backend/MyCookBook.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using MyCookBook.Communication.Requests;
+using MyCookBook.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace MyCookBook.Application.UseCases.User.Register
+{
+    public class RegisterUserValidator : AbstractValidator<RequestRegisterUserJson>
+    {
+        public RegisterUserValidator() 
+        {
+            RuleFor(c => c.Name).NotEmpty().WithMessage(ResourceMessagesErro.EMPTY_NAME);
+            RuleFor(c => c.Email).NotEmpty().WithMessage(ResourceMessagesErro.EMPTY_EMAIL);
+            RuleFor(c => c.Password).NotEmpty().WithMessage(ResourceMessagesErro.EMPTY_PHONE);
+            RuleFor(c => c.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceMessagesErro.INVALID_PASSWORD);
+            RuleFor(c => c.Phone).NotEmpty().WithMessage(ResourceMessagesErro.EMPTY_PASSWORD);
+            When(c => !string.IsNullOrWhiteSpace(c.Email), () =>
+            {
+                RuleFor(c => c.Email).EmailAddress().WithMessage(ResourceMessagesErro.INVALID_EMAIL);
+            });
+            When(c => !string.IsNullOrWhiteSpace(c.Phone), () =>
+            {
+                RuleFor(c => c.Phone).Custom((phone, context) => 
+                {
+                    string phonePattern = "[0-9]{2} [1-9]{1} [0-9]{4}-[0-9]{4}";
+                    var isMatch = Regex.IsMatch(phone, phonePattern);
+                    if (!isMatch)
+                    {
+                        context.AddFailure(new FluentValidation.Results.ValidationFailure(nameof(phone), ResourceMessagesErro.INVALID_PHONE));
+                    }
+                });
+            });
+        }
+    }
+}
