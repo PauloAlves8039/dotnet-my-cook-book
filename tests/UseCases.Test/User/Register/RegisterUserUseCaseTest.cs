72921328f64e3f89a94a7b8586436caffa2d36b4
PauloAlves8039 <pj38alves@gmail.com>
2023-05-16 20:57:51 -0300

    Adicionando teste inicial para a regra de negócio

---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,24 @@
+﻿using MyCookBook.Application.UseCases.User.Register;
+using Utils.ForTest.Mapper;
+using Utils.ForTest.Repositories;
+
+namespace UseCases.Test.User.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public void Validate_Success() 
+        {
+            
+        }
+
+        private RegisterUserUseCase CreateUSeCase() 
+        {
+            var mapper = MapperBuilder.Instance();
+            var repository = UserWriteOnlyRepositoryBuilder.Instance();
+            var unitOfWork = UnitOfWorkBuilder.Instance();
+
+            return new RegisterUserUseCase(repository, mapper, unitOfWork);
+        }
+    }
+}
---
+++ tests/Utils.ForTest/Mapper/MapperBuilder.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MyCookBook.Application.Services.Automapper;
+
+namespace Utils.ForTest.Mapper
+{
+    public class MapperBuilder
+    {
+        public static IMapper Instance()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<AutoMapperConfig>();
+            });
+
+            return configuration.CreateMapper();
+        }
+    }
+}
