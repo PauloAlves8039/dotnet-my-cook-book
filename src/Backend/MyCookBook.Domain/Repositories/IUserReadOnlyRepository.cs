72975b19e07f190c287d1812f9997947802efda7
PauloAlves8039 <pj38alves@gmail.com>
2023-05-20 13:19:20 -0300

    Atualizando nome de função

--- src/Backend/MyCookBook.Domain/Repositories/IUserReadOnlyRepository.cs
+++ src/Backend/MyCookBook.Domain/Repositories/IUserReadOnlyRepository.cs
@@ -5,6 +5,6 @@ namespace MyCookBook.Domain.Repositories
     public interface IUserReadOnlyRepository
     {
         Task<bool> ExistsUserWithEmail(string email);
-        Task<User> Login(string email, string password);
+        Task<User> RecoverPasswordByEmail(string email, string password);
     }
 }
