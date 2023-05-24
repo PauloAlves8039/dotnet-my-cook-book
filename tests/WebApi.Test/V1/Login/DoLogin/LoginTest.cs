99759c74486b8131903e5c8d94f64a51e14a4053
PauloAlves8039 <pj38alves@gmail.com>
2023-05-24 20:07:40 -0300

    Completando teste de unidade para WebApi

--- tests/WebApi.Test/V1/Login/DoLogin/LoginTest.cs
+++ tests/WebApi.Test/V1/Login/DoLogin/LoginTest.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using MyCookBook.Communication.Requests;
+using MyCookBook.Exceptions;
 using System.Net;
 using System.Text.Json;
 
@@ -34,8 +35,71 @@ namespace WebApi.Test.V1.Login.DoLogin
 
             var respondeData = await JsonDocument.ParseAsync(responseBody);
 
-            respondeData.RootElement.GetProperty("nome").GetString().Should().NotBeNullOrWhiteSpace().And.Be(_user.Name);
+            respondeData.RootElement.GetProperty("name").GetString().Should().NotBeNullOrWhiteSpace().And.Be(_user.Name);
             respondeData.RootElement.GetProperty("token").GetString().Should().NotBeNullOrWhiteSpace();
+        }
+
+        [Fact]
+        public async Task Validate_Error_Invalid_Password()
+        {
+            var request = new RequestLoginJson
+            {
+                Email = _user.Email,
+                Password = "invalidPassword"
+            };
+
+            var response = await PostRequest(METHOD, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var respondeData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = respondeData.RootElement.GetProperty("messages").Deserialize<List<string>>();
+            errors.Should().ContainSingle().And.Contain(ResourceErroMessages.INVALID_LOGIN);
+        }
+
+        [Fact]
+        public async Task Validate_Error_Invalid_Email()
+        {
+            var request = new RequestLoginJson
+            {
+                Email = "email@invalid.com",
+                Password = _password
+            };
+
+            var response = await PostRequest(METHOD, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var respondeData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = respondeData.RootElement.GetProperty("messages").Deserialize<List<string>>();
+            errors.Should().ContainSingle().And.Contain(ResourceErroMessages.INVALID_LOGIN);
+        }
+
+        [Fact]
+        public async Task Validate_Error_Invalid_Email_Password()
+        {
+            var request = new RequestLoginJson
+            {
+                Email = "email@invalid.com",
+                Password = "invalidPassword"
+            };
+
+            var response = await PostRequest(METHOD, request);
+
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var respondeData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = respondeData.RootElement.GetProperty("messages").Deserialize<List<string>>();
+            errors.Should().ContainSingle().And.Contain(ResourceErroMessages.INVALID_LOGIN);
         }
     }
 }
