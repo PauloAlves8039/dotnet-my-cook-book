5169f16f4584141741694352474bdf61e41f0c09
PauloAlves8039 <pj38alves@gmail.com>
2023-06-04 12:13:52 -0300

    Criando teste de unidade para validação do usuário

---
+++ tests/Utils.ForTest/Requests/RequestChangePasswordUserBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Bogus;
+using MyCookBook.Communication.Requests;
+using System.Drawing;
+
+namespace Utils.ForTest.Requests
+{
+    public class RequestChangePasswordUserBuilder
+    {
+        public static RequestChangePasswordJson Build(int passwordLength = 10) 
+        {
+            return new Faker<RequestChangePasswordJson>()
+                .RuleFor(c => c.CurrentPassword, f => f.Internet.Password(10))
+                .RuleFor(c => c.NewPassword, f => f.Internet.Password(passwordLength));
+        }
+    }
+}
---
+++ tests/Validators.Test/User/ChangePassword/ChangePasswordValidatorTest.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using MyCookBook.Application.UseCases.User.ChangePassword;
+using MyCookBook.Exceptions;
+using Utils.ForTest.Requests;
+
+namespace Validators.Test.User.ChangePassword
+{
+    public class ChangePasswordValidatorTest
+    {
+        [Fact]
+        public void Validate_Success()
+        {
+            var validador = new ChangePasswordValidator();
+
+            var request = RequestChangePasswordUserBuilder.Build();
+
+            var result = validador.Validate(request);
+
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Validate_Error_Invalid_Password(int passwordLength)
+        {
+            var validador = new ChangePasswordValidator();
+
+            var request = RequestChangePasswordUserBuilder.Build(passwordLength);
+
+            var result = validador.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErroMessages.INVALID_PASSWORD));
+        }
+
+        [Fact]
+        public void Validate_Error_Empty_Password()
+        {
+            var validador = new ChangePasswordValidator();
+
+            var request = RequestChangePasswordUserBuilder.Build();
+            request.NewPassword = string.Empty;
+
+            var result = validador.Validate(request);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().ContainSingle().And.Contain(error => error.ErrorMessage.Equals(ResourceErroMessages.EMPTY_PASSWORD));
+        }
+    }
+}
